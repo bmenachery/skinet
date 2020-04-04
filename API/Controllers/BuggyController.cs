c297562e8081d06e05f271d2dfcf5c0e65504854
bmenachery <bmenachery@gmail.com>
2020-04-04 17:53:36 -0400

    End of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,46 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notFound")]
+        public ActionResult GetNotFoundResult()
+        {
+            var thing = _context.Products.Find(42);
+
+                return NotFound(new ApiResponse(404));
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+            
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(404));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundResult(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
